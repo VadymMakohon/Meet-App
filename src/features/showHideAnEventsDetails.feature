6a8765ba9802da1f2d185dd0155b3178480afeab
VadymMakohon <errorks7@gmail.com>
2024-06-12 00:15:04 +0200

    updated showHide files

--- src/features/showHideAnEventsDetails.feature
+++ src/features/showHideAnEventsDetails.feature
@@ -1,16 +1,16 @@
-Feature: Show and hide events details
+Feature: Show and hide event details
 
     Scenario: An event element is collapsed by default.
         Given the user first opens the app
-        When the user recieves the full list of events (specific for the city or all events)
-        Then all events will colapse by default.
+        When the user receives the full list of events (specific for the city or all events)
+        Then all events will collapse by default.
 
     Scenario: User can expand an event to see its details
         Given the user gets a list of events
         When a user selects an event's details
-        Then the details will show up for that choosen event
+        Then the details will show up for that chosen event.
 
     Scenario: User can collapse an event to hide its details
         Given the user sees the details of an event
         When the user presses a button to hide event's details
-        Then the details of that even will be hidden
+        Then the details of that event will be hidden.
--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,12 +1,12 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import App from '../App';
-import { render, waitFor, within } from '@testing-library/react';
+import { render, waitFor, within, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/showAndHideEventsDetails.feature');
 
 defineFeature(feature, (test) => {
-    //SCENARIO 1
+    // Scenario 1
     test('An event element is collapsed by default.', ({ given, when, then }) => {
         let AppComponent;
         given('the user first opens the app', () => {
@@ -14,27 +14,24 @@ defineFeature(feature, (test) => {
         });
 
         when(
-            'the user recieves the full list of events (specific for the city or all events)',
+            'the user receives the full list of events (specific for the city or all events)',
             async () => {
-                const AppDOM = AppComponent.container.firstChild;
-                const EventListDOM = AppDOM.querySelector('#event-list');
+                const EventListDOM = await screen.findByRole('list', { name: /event-list/i });
 
                 await waitFor(() => {
-                    const EventListItems =
-                        within(EventListDOM).queryAllByRole('listitem');
+                    const EventListItems = within(EventListDOM).getAllByRole('listitem');
                     expect(EventListItems.length).toBe(32);
                 });
             }
         );
 
-        then('all events will colapse by default.', () => {
-            const EventDOM = AppComponent.container.firstChild;
-            const details = EventDOM.querySelector('.details');
+        then('all events will collapse by default.', () => {
+            const details = screen.queryByText('Event Details');
             expect(details).not.toBeInTheDocument();
         });
     });
 
-    // SCENARIO 2
+    // Scenario 2
     test('User can expand an event to see its details', ({
         given,
         when,
@@ -52,19 +49,17 @@ defineFeature(feature, (test) => {
         });
 
         when("a user selects an event's details", async () => {
-            const button = AppComponent.queryAllByText('Show Details')[0];
-
+            const button = screen.getAllByText('Show Details')[0];
             await userEvent.click(button);
         });
 
-        then('the details will show up for that choosen event', () => {
-            const EventDOM = AppComponent.container.firstChild;
-            const details = EventDOM.querySelector('.details');
+        then('the details will show up for that chosen event', () => {
+            const details = screen.getByText('Event Details');
             expect(details).toBeInTheDocument();
         });
     });
 
-    // SCENARIO 3
+    // Scenario 3
     test('User can collapse an event to hide its details', ({
         given,
         when,
@@ -81,21 +76,20 @@ defineFeature(feature, (test) => {
                 expect(eventList[0]).toBeTruthy();
             });
 
-            button = AppComponent.queryAllByText('Show Details')[0];
+            button = screen.getAllByText('Show Details')[0];
             await userEvent.click(button);
 
-            const EventDOM = AppComponent.container.firstChild;
-            const details = EventDOM.querySelector('.details');
+            const details = screen.getByText('Event Details');
             expect(details).toBeInTheDocument();
         });
 
         when("the user presses a button to hide event's details", async () => {
-            await userEvent.click(button);
+            const hideButton = screen.getByText('Hide Details');
+            await userEvent.click(hideButton);
         });
 
-        then('the details of that even will be hidden', () => {
-            const EventDOM = AppComponent.container.firstChild;
-            const details = EventDOM.querySelector('.details');
+        then('the details of that event will be hidden', () => {
+            const details = screen.queryByText('Event Details');
             expect(details).not.toBeInTheDocument();
         });
     });
