f7cc7101bed3e4a3eab009d2227ee707e3403a8e
VadymMakohon <errorks7@gmail.com>
2024-06-13 12:02:43 +0200

    added initialization/config line provided by the Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('1b85422b89774bab8c03a95fbf9e6342').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,9 +15,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
